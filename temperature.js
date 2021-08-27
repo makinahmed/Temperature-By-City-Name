4492e4d99bf10cb402e6ecd67192eb994d90883e
makinahmed <ahmedmakin555@gmail.com>
2021-08-28 03:23:41 +0600

    first commit

---
+++ temperature.js
@@ -0,0 +1,17 @@
+const id = 'cdab67fe7772f7a328daac107ff9a9f7'
+const inputField = document.getElementById('input-field')
+
+const loadCity = () => {
+    let cityName = inputField.value.toLowerCase()
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=cdab67fe7772f7a328daac107ff9a9f7`)
+        .then(res => res.json())
+        .then(data => getTemp(data))
+}
+
+const getTemp = (data) => {
+    document.getElementById('current-temp').innerText = data.main.temp
+    console.log(data.main.temp);
+
+}
+
+
